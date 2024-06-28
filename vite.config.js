9d8fa7bebf022f1ba948009d31a8c698dd77056a
Ayush Paul Tirkey <ayushpaultirkey2001@gmail.com>
2024-06-28 19:07:30 +0530

    v2.0.1
    > Default script added in vite config, to resolve error messages

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,11 @@ export default defineConfig({
     build: {
         outDir: "../dist"
     },
+    optimizeDeps: {
+        entries: [
+            'public/script/app.js',
+        ]
+    },
     plugins: [
         h12VitePlugin()
     ],
