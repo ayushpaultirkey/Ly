195f9c82f6f946ccce2020ee192393e89e8f28fb
Ayush Paul Tirkey <ayushpaultirkey2001@gmail.com>
2025-01-21 06:25:22 +0530

    Update #9
    > code placeholder corrected, as the placeholder >10 were not working correctly.
    > also text code placeholder were copied into keys.

--- plugin/h12.transform.js
+++ plugin/h12.transform.js
@@ -30,6 +30,7 @@ function main(code = "", ignoreCheck = false) {
 
         for(const bracket in brackets) {
             pharsed = pharsed.replace(bracket.replace(/\{|\}/g, ""), brackets[bracket]);
+            console.log(brackets[bracket], bracket);
         }
 
         const haveSubTemplate = pharsed.includes("<>") && pharsed.includes("</>");
@@ -52,7 +53,7 @@ function preFormatBrackets(text = "") {
 
         if(bracket.match(/\s/gm)) {
 
-            const id = `{${PLACEHOLDER_CODE}${index}}`;
+            const id = `{${PLACEHOLDER_CODE}${index}__}`;
 
             text = text.replace(`{${bracket}}`, id);
 
@@ -103,10 +104,14 @@ function pharseText(element = document.body) {
                     textList.push(part.replace(/\{|\}/g, ""));
                 }
                 else {
+
                     textList.push(`\`${part}\``);
-                }
-                if(part.match(/\{[^{}\s]*\}/gm)) {
-                    keyList.push(part);
+
+                    // Only push keys if they are not placeholders
+                    if(part.match(/\{[^{}\s]*\}/gm)) {
+                        keyList.push(part);
+                    }
+
                 }
             }
         }
@@ -118,12 +123,16 @@ function pharseText(element = document.body) {
             textList.push(value.replace(/\{|\}/g, ""));
         }
         else {
+            
             textList.push(`\`${value}\``);
-        }
 
-        if(value.match(/\{[^{}\s]*\}/gm)) {
-            keyList.push(value);
-        }
+            // Only push keys if they are not placeholders
+            if(value.match(/\{[^{}\s]*\}/gm)) {
+                keyList.push(value);
+            };
+
+        };
+
 
     }
 
